512d6931838b33f851de087551adf6f9645ff09c
Василий <naukomania@gmail.com>
2017-07-30 14:17:32 +0300

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('less', function(){
-	gulp.src('./less/*.less')
+gulp.task('less', function() {
+  gulp.src('./less/*.less')
     .pipe(less())
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('less:watch', function(){
-	gulp.watch('./less/*.less', ['less']);
+gulp.task('less:watch', function() {
+  gulp.watch('./less/*.less', ['less']);
+});
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync'], function() {
+  gulp.watch('./**/*.html', browserSync.reload);
+  gulp.watch('css/**/*.css', browserSync.reload);
+  gulp.watch('js/**/*.js', browserSync.reload);
 });
